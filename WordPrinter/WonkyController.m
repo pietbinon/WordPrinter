82b9f3285f31ef74fdfe77797662a41f4c5ad46b
Cory Alder <coryalder@gmail.com>
2016-05-05 01:41:47 -0700

    First commit

---
+++ WordPrinter/WonkyController.m
@@ -0,0 +1,17 @@
+//
+//  WonkyController.m
+//  WordPrinter
+//
+//  Created by Cory Alder on 2016-05-05.
+//  Copyright © 2016 Lighthouse Labs. All rights reserved.
+//
+
+#import "WonkyController.h"
+
+@implementation WonkyController
+
+-(int)printer:(Printer *)printer numberOfTimesToPrint:(NSString *)text {
+    return 10;
+}
+
+@end
